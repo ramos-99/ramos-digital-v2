d8470da345be4e4129f4404f90cc6445461e6576
Martim Ramos <martimcr@gmail.com>
2025-12-25 16:15:46 +0000

    fix: remove duplicate runtime and force dynamic web page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,7 +5,7 @@ import { AboutSection } from "@/app/components/home/AboutSection";
 import { ServicesSection } from "@/app/components/home/ServicesSection";
 import { ProjectsSection } from "@/app/components/home/ProjectsSection";
 
-export const runtime = 'edge';
+
 
 // Main Page Component
 export default function HomePage() {
