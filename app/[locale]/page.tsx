ac784bd1a695434869a7d73f1e7cf7411a452dba
Martim Ramos <martimcr@gmail.com>
2025-12-25 02:44:47 +0000

    fix: make web page dynamic for form submission

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,4 +1,5 @@
-
+export const runtime = 'edge';
+export const dynamic = 'force-dynamic';
 import { HeroSection } from "@/app/components/home/HeroSection";
 import { AboutSection } from "@/app/components/home/AboutSection";
 import { ServicesSection } from "@/app/components/home/ServicesSection";
